d654302ec02b2aaaae6888ae68f730549bbadd56
Thai Villaluna <thaivillaluna@gmail.com>
2024-04-02 12:37:00 -0700

    fix genEd naming to gen_ed

--- src/main/resources/db/migration/V1__create_course_table.sql
+++ src/main/resources/db/migration/V1__create_course_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE course (
     num VARCHAR(63),
     credits INT,
     prof VARCHAR(255),
-    genEd VARCHAR(255),
+    gen_ed VARCHAR(255),
     repeat VARCHAR(255),
     quarters VARCHAR(255),
     preqs VARCHAR(255),
