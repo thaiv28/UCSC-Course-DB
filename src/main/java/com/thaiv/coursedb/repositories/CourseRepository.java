9b666136e50950d815a5ac9111221a9d7b2c2696
Thai Villaluna <thaivillaluna@gmail.com>
2024-04-03 18:56:35 -0700

    add proper annotation

--- src/main/java/com/thaiv/coursedb/repositories/CourseRepository.java
+++ src/main/java/com/thaiv/coursedb/repositories/CourseRepository.java
@@ -1,9 +1,11 @@
 package com.thaiv.coursedb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.thaiv.coursedb.models.Course;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
 
 }
